da6f7f77ee30e8cbae7eba43b6691f3b222fe6f6
Nathan Bessler <NathanBessler@outlook.com>
2023-02-18 18:24:29 -0600

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,184 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DoubleSolenoid.*;
+import edu.wpi.first.wpilibj.shuffleboard.*;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import java.io.FileNotFoundException;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import frc.robot.subsystems.Autonomous;
+import frc.robot.subsystems.SwerveDrive;
+import frc.robot.subsystems.Tracking;
+
+
+
+public class Robot extends TimedRobot {
+  // Define objects and variables
+  private Joystick LeftStick;
+  private Joystick RightStick;
+  private Double RightStickX;
+  private Double RightStickY;
+  private Double RightStickTwist;
+  private Double[] MotorCurrents;
+  private String[] AutoNames;
+  private String PrevAuto;
+
+  public SwerveDrive SwerveDrive;
+  public Tracking Tracking;
+  public Autonomous Autonomous;
+
+  public NetworkTableInstance Inst;
+  public NetworkTable DriverStation;
+  public NetworkTableEntry GyroAng;
+  public SendableChooser<String> AutoChooser;
+
+  private CANSparkMax Intake;
+  private CANSparkMax Indexer;
+  private SparkMaxPIDController IntakePID;
+
+  private Compressor Pump;
+  private DoubleSolenoid Intake1;
+
+  @Override
+  public void robotInit() {
+    Inst = NetworkTableInstance.getDefault();
+
+    AutoChooser = new SendableChooser<String>();
+    AutoNames = Filesystem.getDeployDirectory().toPath().resolve("output/paths").toFile().list();
+    for (Integer Index = 0; Index <= AutoNames.length - 1; Index++) {
+      AutoChooser.addOption(AutoNames[Index], AutoNames[Index]);
+    }
+    // hehe funne number
+    SmartDashboard.putData("AutoChooser", AutoChooser);
+    
+    // Assign joysticks to the "LeftStick" and "RightStick" objects
+    LeftStick = new Joystick(0);
+    RightStick = new Joystick(1);
+
+    Intake = new CANSparkMax(9, MotorType.kBrushed);
+    Indexer = new CANSparkMax(11, MotorType.kBrushless);
+
+    Pump = new Compressor(0, PneumaticsModuleType.CTREPCM);
+    Intake1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    Intake1.set(Value.kReverse);
+
+    // Instantiate an object for each class
+    SwerveDrive = new SwerveDrive();
+    Tracking = new Tracking(SwerveDrive);
+    Autonomous = new Autonomous(SwerveDrive, Tracking);
+
+    // Call SwerveDrive methods, their descriptions are in the SwerveDrive.java file
+    SwerveDrive.initMotorControllers(1, 2, 7, 8, 5, 6, 3, 4);
+    SwerveDrive.setPID(0.000175, 0.0000007, 0.0000001, 0.0, 8.0, 0.01, 0.01);
+    SwerveDrive.initKinematicsAndOdometry();
+    PrevAuto = AutoChooser.getSelected();
+    Autonomous.AutoFile = AutoChooser.getSelected();
+    if (AutoChooser.getSelected() != null) {
+    try {
+        Autonomous.initTrajectory();
+      } catch (FileNotFoundException e) {
+        System.out.println("AUTO NOT FOUND");
+      }
+    }
+    Pump.enableDigital();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+    if (PrevAuto != AutoChooser.getSelected()) {
+      Autonomous.AutoFile = AutoChooser.getSelected();
+      try {
+        Autonomous.initTrajectory();
+      } catch (FileNotFoundException e) {
+        System.out.println("AUTO NOT FOUND");
+      }
+      PrevAuto = AutoChooser.getSelected();
+    }
+  }
+ 
+
+  @Override
+  public void teleopPeriodic() {
+    // Assign stick inputs to variables, to prevent discrepancies
+    RightStickX = RightStick.getX();
+    RightStickY = RightStick.getY();
+    RightStickTwist = RightStick.getRawAxis(3);
+
+    // Create deadzones on the joysticks, to prevent stick drift
+    if (Math.abs(RightStickX) < 0.1) {
+      RightStickX = 0.0;
+    }
+    if (Math.abs(RightStickY) < 0.1) {
+      RightStickY = 0.0;
+    }
+    if (Math.abs(RightStickTwist) < 0.2) {
+      RightStickTwist = 0.0;
+    }
+
+    if (RightStick.getRawButton(6)) {
+      Tracking.centerOnCone();
+    }
+    else {
+      // Call swerveDrive() method, to do all the math and outputs for swerve drive
+      SwerveDrive.swerveDrive(RightStickX * 2, (RightStickY * -2), (RightStickTwist * 2.5), (1 - ((RightStick.getZ() + 1) / 2)), (1 - ((LeftStick.getZ() + 1) / 2)));
+      SwerveDrive.setVariablesAndOptimize();
+      SwerveDrive.setSwerveOutputs();
+    }
+
+    SmartDashboard.putNumber("Gyro", SwerveDrive.GyroRotation2d.unaryMinus().getDegrees());
+
+    MotorCurrents = new Double[] {SwerveDrive.FrontLeft.Drive.getOutputCurrent(), SwerveDrive.FrontRight.Drive.getOutputCurrent(), SwerveDrive.BackLeft.Drive.getOutputCurrent(), SwerveDrive.BackRight.Drive.getOutputCurrent()};
+    SmartDashboard.putNumberArray("RobotDrive Motors", MotorCurrents);
+  
+    if (RightStick.getRawButton(2) == true) {
+      SwerveDrive.Gyro.reset();
+    }
+    if (RightStick.getRawButton(3)) {
+      Intake.set(1);
+    }
+    else {
+      Intake.set(0);
+    }
+    if (RightStick.getRawButton(5)) {
+      Indexer.set(.15);
+    }
+    else {
+      Indexer.set(0);
+    }
+    if (RightStick.getRawButtonPressed(4)) {
+      Intake1.toggle();
+    }
+  }
+
+  //Autonomous right away
+  @Override
+  public void autonomousInit(){
+    System.out.println("Start Y: " + SwerveDrive.Odometry.getPoseMeters().getY());
+  }
+
+  //Autonomous repeat
+  @Override
+  public void autonomousPeriodic(){ 
+    Autonomous.runAutonomous();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Autonomous.java
@@ -0,0 +1,197 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.nio.file.Path;
+import java.util.function.Supplier;
+
+import org.ejml.ops.ReadCsv;
+
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.trajectory.constraint.SwerveDriveKinematicsConstraint;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.io.*;
+
+public class Autonomous extends SubsystemBase {
+  public Tracking Tracking;
+  private SwerveDrive Swerve;
+  public String AutoFile;
+  private Path TrajectoryPath;
+  private File TrajectoryFile;
+  private TrajectoryConfig TrajectoryConfig;
+  private Trajectory PlaceFirstObjectTrajectory;
+  private Trajectory GetSecondObjectTrajectory;
+  private Trajectory PlaceSecondObjectTrajectory;
+  private Trajectory ToChargerTrajectory;
+  private SwerveDriveKinematicsConstraint SwerveDriveMaxSpeed;
+  private Constraints PIDConstraints;
+  private Scanner AutoReader;
+  private List<String> Lines;
+  private List<String> CurrentLine;
+  private List<Double> XPositions;
+  private List<Double> YPositions;
+  private List<Double> Angles;
+  private Boolean HasChargerAuto = false;
+  private SwerveControllerCommand PlaceFirstObject;
+  private SwerveControllerCommand GetSecondObject;
+  private SwerveControllerCommand PlaceSecondObject;
+  private SwerveControllerCommand ToCharger;
+  private Integer AutoStage;
+  private Boolean IsScheduled = false;
+
+  public Autonomous(SwerveDrive SwerveDrive, Tracking Track) {
+    Swerve = SwerveDrive;
+    Tracking = Track;
+    Lines = new ArrayList<String>();
+    CurrentLine = new ArrayList<String>();
+    XPositions = new ArrayList<Double>();
+    YPositions = new ArrayList<Double>();
+    Angles = new ArrayList<Double>();
+    AutoStage = 0;
+  }
+
+  public void initTrajectory() throws FileNotFoundException {
+    TrajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("output/" + AutoFile);
+    System.out.println(TrajectoryPath.toString());
+    TrajectoryFile = TrajectoryPath.toFile();
+    AutoReader = new Scanner(TrajectoryFile);
+    SwerveDriveMaxSpeed = new SwerveDriveKinematicsConstraint(Swerve.Kinematics, 1);
+    PIDConstraints = new Constraints(1, 2);
+    TrajectoryConfig = new TrajectoryConfig(1, 1).setKinematics(Swerve.Kinematics).addConstraint(SwerveDriveMaxSpeed);
+    Lines.clear();
+    CurrentLine.clear();
+    XPositions.clear();
+    YPositions.clear();
+    Angles.clear();
+    AutoStage = 0;
+    Swerve.Gyro.reset();
+
+    if (AutoReader.hasNextLine()) {
+      AutoReader.nextLine();
+    }
+    while (AutoReader.hasNextLine()) {
+      Lines.add(AutoReader.nextLine());
+    }
+    for (Integer Index = 0; Index <= Lines.size() - 1; Index++) {
+      CurrentLine.addAll(Arrays.asList(Lines.get(Index).split(",")));
+      XPositions.add(Double.parseDouble(CurrentLine.get(0)));
+      YPositions.add(Double.parseDouble(CurrentLine.get(1)));
+      System.out.println(CurrentLine.get(1));
+      if (TrajectoryPath.toString().contains("Red")) {
+        Angles.add(Math.atan2(Double.parseDouble(CurrentLine.get(3)), Double.parseDouble(CurrentLine.get(2))) + Math.PI);
+      }
+      if (TrajectoryPath.toString().contains("Blue")) {
+        Angles.add(Math.atan2(Double.parseDouble(CurrentLine.get(3)), Double.parseDouble(CurrentLine.get(2))));
+      }
+      if (Index == 6) {
+        HasChargerAuto = true;
+      }
+      CurrentLine.clear();
+    }
+    AutoReader.close();
+    Swerve.Odometry.resetPosition(new Rotation2d(Angles.get(0)), new SwerveModulePosition[] {Swerve.FrontRight.getPosition(), Swerve.FrontLeft.getPosition(), Swerve.BackLeft.getPosition(), Swerve.BackRight.getPosition()}, new Pose2d(new Translation2d(XPositions.get(0), YPositions.get(0)), new Rotation2d(0)));
+
+    PlaceFirstObjectTrajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(new Translation2d(XPositions.get(0), YPositions.get(0)), new Rotation2d(Angles.get(0))), List.of(), new Pose2d(new Translation2d(XPositions.get(1), YPositions.get(1)), new Rotation2d(Angles.get(1))), TrajectoryConfig);
+    //GetSecondObjectTrajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(new Translation2d(XPositions.get(1), YPositions.get(1)), new Rotation2d(Angles.get(1))), List.of(new Translation2d(XPositions.get(2), YPositions.get(2))), new Pose2d(new Translation2d(XPositions.get(3), YPositions.get(3)), new Rotation2d(Angles.get(3))), TrajectoryConfig);
+    //PlaceSecondObjectTrajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(new Translation2d(XPositions.get(3), YPositions.get(3)), new Rotation2d(Angles.get(3))), List.of(new Translation2d(XPositions.get(4), YPositions.get(4))), new Pose2d(new Translation2d(XPositions.get(5), YPositions.get(5)), new Rotation2d(Angles.get(5))), TrajectoryConfig);
+    //ToChargerTrajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(new Translation2d(XPositions.get(5), YPositions.get(5)), new Rotation2d(Angles.get(5))), List.of(), new Pose2d(new Translation2d(XPositions.get(6), YPositions.get(6)), new Rotation2d(Angles.get(6))), TrajectoryConfig);
+
+    PlaceFirstObject = new SwerveControllerCommand(PlaceFirstObjectTrajectory, Swerve::getPose, Swerve.Kinematics, new HolonomicDriveController(new PIDController(1, 0, 0), new PIDController(1, 0, 0), new ProfiledPIDController(1, 0, 0, PIDConstraints)), Swerve::setModuleStates, Swerve);
+    //GetSecondObject = new SwerveControllerCommand(GetSecondObjectTrajectory, Swerve::getPose, Swerve.Kinematics, new HolonomicDriveController(new PIDController(1, 0, 0), new PIDController(1, 0, 0), new ProfiledPIDController(1, 0, 0, PIDConstraints)), Swerve::setModuleStates, Swerve);
+    //PlaceSecondObject = new SwerveControllerCommand(PlaceSecondObjectTrajectory, Swerve::getPose, Swerve.Kinematics, new HolonomicDriveController(new PIDController(1, 0, 0), new PIDController(1, 0, 0), new ProfiledPIDController(1, 0, 0, PIDConstraints)), Swerve::setModuleStates, Swerve);
+    //ToCharger = new SwerveControllerCommand(ToChargerTrajectory, Swerve::getPose, Swerve.Kinematics, new HolonomicDriveController(new PIDController(1, 0, 0), new PIDController(1, 0, 0), new ProfiledPIDController(1, 0, 0, PIDConstraints)), Swerve::setModuleStates, Swerve);
+  }
+
+  public void runAutonomous() {
+    if (AutoStage == 0) {
+      if (!IsScheduled) {
+        PlaceFirstObject.andThen(() -> Swerve.stop()).schedule();
+        IsScheduled = true;
+      }
+      if (PlaceFirstObject.isFinished()) {
+        AutoStage = 1;
+        IsScheduled = false;
+      }
+    }
+    /**
+    if (AutoStage == 1) {
+      //Extend arm
+      //Line up with Limelight
+      //Drop object
+      //Retract arm
+      AutoStage = 2;
+    }
+    if (AutoStage == 2) {
+      if (!IsScheduled) {
+        GetSecondObject.andThen(() -> Swerve.stop()).schedule();
+        IsScheduled = true;
+      }
+      if (GetSecondObject.isFinished()) {
+        AutoStage = 3;
+        IsScheduled = false;
+      }
+    }
+    if (AutoStage == 3) {
+      //Extend intake
+      //Start motors
+      //Pick up with Limelight
+      //Stop motors
+      //Retract intake
+      AutoStage = 4;
+    }
+    if (AutoStage == 4) {
+      if (!IsScheduled) {
+        PlaceSecondObject.andThen(() -> Swerve.stop()).schedule();
+        IsScheduled = true;
+      }
+      if (PlaceSecondObject.isFinished()) {
+        AutoStage = 5;
+        IsScheduled = false;
+      }
+    }
+    if (AutoStage == 5) {
+      //Extend arm
+      //Line up with Limelight
+      //Drop object
+      //Retract arm
+      AutoStage = 6;
+    }
+    if (AutoStage == 6 & HasChargerAuto) {
+      if (!IsScheduled) {
+        ToCharger.andThen(() -> Swerve.stop()).schedule();
+        IsScheduled = true;
+      }
+      if (ToCharger.isFinished()) {
+        AutoStage = 7;
+        IsScheduled = false;
+      }
+    }
+    if (AutoStage == 7) {
+      //Auto balance on charger
+    }
+    */
+  }
+}
